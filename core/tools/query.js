17dd11bf580078c45d15ab9c50547d9c11b54af6
markthree <1801982702@qq.com>
2022-01-15 15:56:34 +0800

    fix(tool-query): 规避tree-shaking

--- core/tools/query.js
+++ core/tools/query.js
@@ -1,3 +1,7 @@
+// 避免被 tree shaking 掉
+const createQl = (vm, method, selector) =>
+	uni.createSelectorQuery().in(vm)[method](selector)
+
 /**
  * 查询节点信息
  * https://uniapp.dcloud.io/api/ui/nodes-info?id=selectorqueryselectall
@@ -5,11 +9,7 @@
 export default (vm, selector, more = false) => {
 	const queryHandle = resolve => {
 		const method = more ? 'selectAll' : 'select'
-		let ql = uni
-			.createSelectorQuery()
-			.in(vm)
-			[method](selector)
-
+		let ql = createQl(vm, method, selector)
 		ql.boundingClientRect(rect => resolve(rect)).exec()
 	}
 	return new Promise(queryHandle)
